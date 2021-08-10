bb6b88f3d29f3e243f818bfc7345227a96d87a34
joshsimon-bit <83602394+joshsimon-bit@users.noreply.github.com>
2021-08-10 16:31:28 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2 className="offer">Do you want to learn to code?</h2>
+      <h3 className="intro">Red or Blue Pill?</h3>
+      <div className="thePills">
+        <button className="redPill">Red Pill</button>
+        <button className="bluePill"> Blue Pill</button>
+      </div>
+    </div>
+  );
+}
